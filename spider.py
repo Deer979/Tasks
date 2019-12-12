10933f54e77d2a4eeeb388a36562b77217dd96dc
Deer979 <57405338+Deer979@users.noreply.github.com>
2019-12-12 21:49:12 +0800

    爬取豆瓣新书

--- spider.py
+++ spider.py
@@ -11,39 +11,63 @@ class doubanxingshu(object):
         self.header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:71.0) Gecko/20100101 Firefox/71.0'}
         self.bookname = []
         self.author = []
+        self.fout = open('豆瓣新书速递.html','w')
     
     def get_data(self):
         html = requests.get(self.url,headers=self.header)
+        content = html.text
         print(html.status_code)
-        html.encoding = 'utf-8'
         soup = BeautifulSoup(html.text,'lxml')
-        shuming = soup.select('.article > ul:nth-child(2) > li > div:nth-child(2) > p:nth-child(3)')
-        zhuozhe = soup.select('.article > ul:nth-child(2) > li > div:nth-child(2) > h2:nth-child(1) > a:nth-child(1)')
+        shuming1 = soup.select('.article > ul:nth-child(2) > li > div:nth-child(2) > h2:nth-child(1) > a:nth-child(1)')
+        zhuozhe1 = soup.select('.article > ul:nth-child(2) > li > div:nth-child(2) > p:nth-child(3)')
+        jianjie1 = soup.select('.article > ul:nth-child(2) > li > div:nth-child(2) > p:nth-child(4)')
+        shuming2 = soup.select('ul.pl20 > li > div:nth-child(2) > h2:nth-child(1) > a:nth-child(1)')
+        zhuozhe2 = soup.select('ul.pl20 > li > div:nth-child(2) > p:nth-child(3)')
+        jianjie2 = soup.select('ul.pl20 > li > div:nth-child(2) > p:nth-child(4)')
         print(html.apparent_encoding)
-        author_bookname = []
+        b_a_j = []
 
-        for x,y in zip(zhuozhe,shuming):
+        for x,y,z in zip(shuming1,zhuozhe1,jianjie1):
             print(x.text)
             print(y.text)
-            author_bookname.append(x.text)
-            author_bookname.append(y.text)
+            print(z.text)
+            b_a_j.append(x.text)
+            b_a_j.append(y.text)
+            b_a_j.append(z.text)
+            
+        for x,y,z in zip(shuming2,zhuozhe2,jianjie2):
+            print(x.text)
+            print(y.text)
+            print(z.text)
+            b_a_j.append(x.text)
+            b_a_j.append(y.text)
+            b_a_j.append(z.text)
+           
+        
+        step = 3
+        bookname_author_recommend = [b_a_j[i:i+step] for i in range(0,len(b_a_j),step)]
     
     #形成文件
         fout = open('豆瓣新书速递.html','w')
+        self.fout.write(content.encode("gbk","ignore").decode("gbk","ignore"))
         
         fout.write("<!DOCTYPE html>\n")
         fout.write("<html lang='en'>\n")
         fout.write("<mate charset='UTF-8'>")
         fout.write("<head>")
         fout.write("<body style='text-aling:center;'>\n")
-        fout.write("<table style='margin:auto;'>\n")
+        fout.write("<h1 style='position:relative;top:20px;text-align:center;'>豆瓣新书速递</h1>")
+        fout.write("<div style='position:relative;margin:auto;margin-top:5px;'>\n")
 
-        for data in author_bookname:
-            fout.write("<tr>\n")
-            fout.write("<td style='text-align:center;'>%s</td>\n"%data)
-            fout.write("</tr>\n")
+        i = 0
+        for data in bookname_author_recommend:
+            fout.write("<div style='position:relative;margin:top:100px;'>\n")
+            for data in bookname_author_recommend[i]:
+                fout.write("<p style='text-align:center;'>%s</p>\n"%data.encode(encoding='gbk',errors='ignore').decode("gbk","ignore"))
+            i = i+1
+            fout.write("</div>\n")
 
-        fout.write("</table>\n")
+        fout.write("</div>\n")
         fout.write("</body>\n")
         fout.write("</head>")
         fout.write("</html>\n")
@@ -53,4 +77,3 @@ if __name__ == "__main__":
     root_url = 'https://book.douban.com/latest?icn=index-latestbook-all'
     wangye = doubanxingshu()
     wangye.get_data()
-
